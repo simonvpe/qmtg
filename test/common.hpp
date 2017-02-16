ffac38d2c0270855c86f074ec0dbf9947254094b
Simon Pettersson <simon.v.pettersson@gmail.com>
2017-02-16 22:34:40 +0100

    Refactoring

--- test/common.hpp
+++ test/common.hpp
@@ -17,10 +17,9 @@ namespace MTG {
 
     ////////////////////////////////////////////////////////////////////////////
     struct GameHandle : public ex::Entity {
-        GameHandle(ex::Entity e = {})
-        : ex::Entity{e}
-        {}
+        GameHandle(ex::Entity e = {}) : ex::Entity{e} {}
         auto operator->() { return component<Game>(); }
+        const auto operator->() const { return component<const Game>(); }
     };
     ////////////////////////////////////////////////////////////////////////////
 
@@ -37,10 +36,6 @@ namespace MTG {
         std::bitset<2> flags;
         int            life;
         int            startingHandSize;
-        bool active() const { return flags[ACTIVE]; }
-        void setActive()    { flags[ACTIVE] = true; }
-        bool ready()  const { return flags[READY];  }
-        void setReady()     { flags[READY]  = true; }
     };
     ////////////////////////////////////////////////////////////////////////////
 
@@ -50,9 +45,32 @@ namespace MTG {
         : ex::Entity{e}
         {}
         auto operator->() { return component<Player>(); }
+        const auto operator->() const { return component<const Player>(); }
     };
+    using PlayerVector = const std::vector<PlayerHandle>;
+    using MutablePlayerVector = std::vector<PlayerHandle>;
     ////////////////////////////////////////////////////////////////////////////
 
+    ////////////////////////////////////////////////////////////////////////////
+    namespace player {
+        inline bool isActive(const PlayerHandle& player) {
+            return player->flags[Player::ACTIVE];
+        }
+
+        inline void setActive(PlayerHandle& player, bool value = true) {
+            player->flags[Player::ACTIVE] = value;
+        }
+
+        inline bool isReady(const PlayerHandle& player) {
+            return player->flags[Player::READY];
+        }
+        
+        inline void setReady(PlayerHandle& player, bool value = true) {
+            player->flags[Player::READY] = value;
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////
+    
     ////////////////////////////////////////////////////////////////////////////
     struct Card {
         enum { MAX_NAME_LENGTH = 20};
@@ -60,8 +78,6 @@ namespace MTG {
         char         name[MAX_NAME_LENGTH];
         Zone         zone;
         PlayerHandle player;
-        bool isInLibrary() const { return LIBRARY == zone; }
-        bool isInHand()    const { return HAND    == zone; }
     };
     ////////////////////////////////////////////////////////////////////////////
 
@@ -70,33 +86,33 @@ namespace MTG {
         CardHandle(ex::Entity e = {})
         : ex::Entity{e}
         {}
-        auto operator->()
-        { return component<Card>(); }
+        auto operator->() { return component<Card>(); }
+        const auto operator->() const { return component<const Card>(); } 
         
-        bool operator==(const char *rhs)
+        bool operator==(const char *rhs) const
         { return 0 == std::strcmp(rhs,(*this)->name); }
     };
+    using CardVector = const std::vector<CardHandle>;
+    using MutableCardVector = std::vector<CardHandle>;
     ////////////////////////////////////////////////////////////////////////////
 
     ////////////////////////////////////////////////////////////////////////////
     namespace card {
-        inline auto make(
-        ex::EntityManager& mgr,
-        PlayerHandle       player,
-        std::string        name) {
-            auto e = CardHandle{mgr.create()};
-            auto card = e.assign<Card>();
-            card->player = player;
-            std::strncpy(card->name, name.c_str(), Card::MAX_NAME_LENGTH);
-            return e;
-        }
-
-        inline void move(
-        CardHandle card,
-        Card::Zone zone) {
+        inline void setZone(CardHandle card, Card::Zone zone) {
             card->zone = zone;
         }
-        
+
+        inline Card::Zone getZone(const CardHandle& card) {
+            return card->zone;
+        }
+
+        inline bool isInHand(const CardHandle &card) {
+            return card->zone == Card::HAND;
+        }
+
+        inline bool isInLibrary(const CardHandle &card) {
+            return card->zone == Card::LIBRARY;
+        }
     }
     ////////////////////////////////////////////////////////////////////////////
 
@@ -104,23 +120,17 @@ namespace MTG {
     class RandomGenerator {
     public:
         ////////////////////////////////////////////////////////////////////////
-        RandomGenerator()
-        : m_gen{m_rd()}
-        {}
+        RandomGenerator() : m_gen{m_rd()} {}
         ////////////////////////////////////////////////////////////////////////
 
         ////////////////////////////////////////////////////////////////////////
-        virtual void
-        shuffle(
-        gsl::span<const char*> container) {
+        virtual void shuffle(gsl::span<const char*> container) {
             std::shuffle(container.begin(), container.end(), m_gen);   
         }
         ////////////////////////////////////////////////////////////////////////
 
         ////////////////////////////////////////////////////////////////////////
-        virtual void
-        shuffle(
-        gsl::span<PlayerHandle> container) {
+        virtual void shuffle(gsl::span<PlayerHandle> container) {
             std::shuffle(container.begin(), container.end(), m_gen);   
         }
         ////////////////////////////////////////////////////////////////////////
@@ -130,179 +140,133 @@ namespace MTG {
         std::random_device m_rd;
         std::mt19937       m_gen;
     };
+    using pRandomGenerator = std::unique_ptr<RandomGenerator>;
     ////////////////////////////////////////////////////////////////////////////
     
     ////////////////////////////////////////////////////////////////////////////
+    
     class Context : public ex::EntityX {
     public:
         ////////////////////////////////////////////////////////////////////////
-        Context(std::unique_ptr<RandomGenerator> random)
-        : m_random{std::move(random)}
-        {}
+        Context(pRandomGenerator random) : m_random{std::move(random)} {}
         ////////////////////////////////////////////////////////////////////////
 
         ////////////////////////////////////////////////////////////////////////
-        Context()
-        : Context(std::make_unique<RandomGenerator>())
-        {}
+        Context() : Context(std::make_unique<RandomGenerator>()) {}
         ////////////////////////////////////////////////////////////////////////
         
         ////////////////////////////////////////////////////////////////////////
-        auto startGame() {
+        auto makeGame() {
             auto game = GameHandle{entities.create()};
             game.assign<Game>();
-
-            auto player1 = PlayerHandle{entities.create()};
-            player1.assign<Player>();
-            player1->game            = game;
-            player1->flags           = 0;
-            player1->life             = 20;
-            player1->startingHandSize = 7;
-            
-            auto player2 = PlayerHandle{entities.create()};
-            player2.assign<Player>();
-            player2->game             = game;            
-            player2->flags            = 0;
-            player2->life             = 20;
-            player2->startingHandSize = 7;
-
-            player1->other = player2;
-            player2->other = player1;
-            
-            return std::make_tuple(player1, player2);
+            return game;
         }
         ////////////////////////////////////////////////////////////////////////
 
         ////////////////////////////////////////////////////////////////////////
-        bool
-        connected(
-        PlayerHandle player) {
+        inline auto makePlayer(GameHandle& game) {
+            auto player = PlayerHandle{entities.create()};
+            player.assign<Player>();
+            player->game             = game;
+            player->flags            = 0;
+            player->life             = 20;
+            player->startingHandSize = 7;
+            return player;
+        }        
+        ////////////////////////////////////////////////////////////////////////
+
+        ////////////////////////////////////////////////////////////////////////
+        inline auto makeCard(PlayerHandle player, std::string name) {
+            auto entity = CardHandle{entities.create()};
+            auto card   = entity.assign<Card>();
+            card->player = player;
+            std::strncpy(card->name, name.c_str(), Card::MAX_NAME_LENGTH);
+            return entity;
+        }
+        ////////////////////////////////////////////////////////////////////////
+        
+        ////////////////////////////////////////////////////////////////////////
+        bool isConnected(PlayerHandle player) {
             return player.component<Socket>().valid();
         }
         ////////////////////////////////////////////////////////////////////////
 
         ////////////////////////////////////////////////////////////////////////
-        void
-        connect(
-        PlayerHandle player) {
-            if(connected(player)) return;
+        void connect(PlayerHandle player) {
+            if(isConnected(player)) return;
             player.assign<Socket>();
         }
         ////////////////////////////////////////////////////////////////////////
 
         ////////////////////////////////////////////////////////////////////////
-        void
-        setDeck(
-        PlayerHandle player,
-        gsl::span<const char*> cards) {
+        void setDeck(PlayerHandle player, gsl::span<const char*> cards) {
             eachCard(player,[](auto card) { card.destroy(); });
             std::vector<const char*> mcards(cards.begin(), cards.end());
             m_random->shuffle(mcards);
             for(auto i = 0 ; i < mcards.size() ; ++i) {
-                auto c = card::make(entities,player,mcards[i]);
+                auto c = makeCard(player,mcards[i]);
                 auto zone = (i < player->startingHandSize)
                     ? Card::HAND
                     : Card::LIBRARY;
-                card::move(c, zone);
+                card::setZone(c, zone);
             }
         }
         ////////////////////////////////////////////////////////////////////////
 
         ////////////////////////////////////////////////////////////////////////
-        std::vector<CardHandle>
-        library(
-        PlayerHandle player) {
-            std::vector<CardHandle> cards;
+        CardVector getLibrary(PlayerHandle player) {
+            MutableCardVector cards;
             cards.reserve(60);
             eachCard(player, [&](CardHandle card) {
-                if(card->isInLibrary()) cards.push_back(card);
+                if(card::isInLibrary(card)) cards.push_back(card);
             });
-            return cards;
+            return (CardVector)cards;
         }
         ////////////////////////////////////////////////////////////////////////
 
         ////////////////////////////////////////////////////////////////////////
-        std::vector<CardHandle>
-        hand(
-        PlayerHandle player) {
-            std::vector<CardHandle> cards;
+        CardVector getHand(PlayerHandle player) {
+            MutableCardVector cards;
             cards.reserve(7);
             eachCard(player,[&](CardHandle card) {
-                    if(card->isInHand()) {
-                        cards.push_back(card);
-                    }
+                if(card::isInHand(card)) {
+                    cards.push_back(card);
+                }
             });
-            return cards;
+            return (CardVector)cards;
         }        
         ////////////////////////////////////////////////////////////////////////
 
         ////////////////////////////////////////////////////////////////////////
-        void
-        setRandomGenerator(
-        std::unique_ptr<RandomGenerator> generator) {
-            m_random = std::move(generator);
-        }
-        ////////////////////////////////////////////////////////////////////////
-
-        ////////////////////////////////////////////////////////////////////////
-        auto
-        getPlayers(GameHandle game) {
-            std::vector<PlayerHandle> players;
+        auto getPlayers(GameHandle game) {
+            MutablePlayerVector players;
             players.reserve(2);
             eachPlayer(game,[&](auto playerHandle) {
                 players.push_back(playerHandle);
             });
-            return players;            
+            return (PlayerVector)players;            
         }
-        ////////////////////////////////////////////////////////////////////////
-
-        ////////////////////////////////////////////////////////////////////////
-        auto
-        setReady(PlayerHandle player) {
-            auto game = player->game;
-            player->setReady();
-            bool allReady = true;
-            eachPlayer(game,[&](auto playerHandle) {
-                allReady = allReady && playerHandle->ready();
-            });
-            if( allReady ) initializeGame(game);
-        }
-        ////////////////////////////////////////////////////////////////////////
-
-        ////////////////////////////////////////////////////////////////////////
-        void
-        setMulligan(
-        PlayerHandle player,
-        bool selection) {
-            if(selection)
-        }
-        ////////////////////////////////////////////////////////////////////////
-        
+        ////////////////////////////////////////////////////////////////////////    
         
     protected:
         ////////////////////////////////////////////////////////////////////////
-        void
-        initializeGame(GameHandle game) {
+        void initializeGame(GameHandle game) {
             randomizeActivePlayer(game);
             game->started = true;
         }
         ////////////////////////////////////////////////////////////////////////
 
         ////////////////////////////////////////////////////////////////////////
-        void
-        randomizeActivePlayer(
-        GameHandle game) {
+        void randomizeActivePlayer(GameHandle game) {
             auto players = getPlayers(game);
             m_random->shuffle(players);
-            (*players.begin())->setActive();
+            player::setActive(*players.begin(), true);
         }
         ////////////////////////////////////////////////////////////////////////
 
         ////////////////////////////////////////////////////////////////////////
-        void
-        eachPlayer(
-        GameHandle                          game,
-        std::function<void(PlayerHandle)>&& func) {
+        template<typename TFunc>
+        void eachPlayer(GameHandle game, TFunc&& func) {
             entities.each<Player>([&](PlayerHandle playerHandle, auto&) {
                 if(playerHandle->game == game) func(playerHandle);
             });
@@ -310,31 +274,16 @@ namespace MTG {
         ////////////////////////////////////////////////////////////////////////
 
         ////////////////////////////////////////////////////////////////////////
-        void
-        eachCard(
-        PlayerHandle                      player,
-        std::function<void(CardHandle)>&& func) {
+        template<typename TFunc>
+        void eachCard(PlayerHandle player, TFunc&& func) {
             entities.each<Card>([&](CardHandle cardHandle, auto&) {
                 if(cardHandle->player == player) func(cardHandle);
             });
         }
         ////////////////////////////////////////////////////////////////////////
-
-        ////////////////////////////////////////////////////////////////////////
-        CardHandle
-        firstCard(
-        PlayerHandle player,
-        Card::Zone   zone) {
-            entityx::ComponentHandle<Card> card;
-            for(auto entity : entities.entities_with_components(card)) {
-                if(card->player == player) return {entity};
-            }
-            return {};
-        }
-        ////////////////////////////////////////////////////////////////////////
         
     private:
-        std::unique_ptr<RandomGenerator> m_random;
+        pRandomGenerator m_random;
     };
     ////////////////////////////////////////////////////////////////////////////
 }
